36049946492b3291cc125fea2811253ae7dce140
Diego <dgvlpt@gmail.com>
2014-05-01 18:52:50 -0300

    corrected the IOException

--- src/dgvolpato/SoundCloud/pi/FindSoundCloudLogo.java
+++ src/dgvolpato/SoundCloud/pi/FindSoundCloudLogo.java
@@ -6,23 +6,17 @@ import java.util.Scanner;
  * Created by Diego on 30/04/2014.
  */
 
-//TODO check if file finishes with the following sequence: 282046127557151713951152750455
+
 public class FindSoundCloudLogo {
     public static void main (String[] args) {
         System.out.println("Hello, SoundCloud");
 
-        //read file
         String file = "C:\\pi-billion.txt";
         String fileTest = "C:\\piTest.txt";
         int count = 0;
 
         try(BufferedReader br = new BufferedReader(new FileReader(file),20)) {
-            /**for(String line; (line = br.readLine()) != null && count < 10; count++ ) {
-                // process the line.
-                System.out.println(line);
-            }
-            // line is not visible here.**/
-            for(String line; (line = readExactly(br, 100)) != null ;  ) {
+            for(String line; (line = readExactly(br, 50000)) != null ;  ) {
                 System.out.println(line);
             }
         }
@@ -37,7 +31,8 @@ public class FindSoundCloudLogo {
         while (offset < length) {
             int charsRead = reader.read(chars, offset, length - offset);
             if (charsRead <= 0) {
-                throw new IOException("Stream terminated early");
+                //throw new IOException("Stream terminated early");
+                return null;
             }
             offset += charsRead;
         }
