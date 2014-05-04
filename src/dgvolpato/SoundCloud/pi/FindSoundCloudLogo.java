32973df6c9162c28f09ddca0c357574c196d6481
Diego <dgvlpt@gmail.com>
2014-05-04 20:00:59 -0300

    finished calculation with simple delta function!

--- src/dgvolpato/SoundCloud/pi/FindSoundCloudLogo.java
+++ src/dgvolpato/SoundCloud/pi/FindSoundCloudLogo.java
@@ -8,11 +8,6 @@ import static dgvolpato.SoundCloud.pi.Constants.*;
 /**
  * Created by Diego on 30/04/2014.
  */
-//TODO work on case of numbers between the groups of 50000
-//TODO review logic of the arrays used as parameters
-//TODO develop and test delta function
-//TODO review case of topTen array still empty or not plainly full
-//TODO review structure of FoundLogos class
 
 public class FindSoundCloudLogo {
 
@@ -29,7 +24,7 @@ public class FindSoundCloudLogo {
 
         try(BufferedReader br = new BufferedReader(new FileReader(FILE_DIRECTORY))) {
             String lineStart = readExactly(br, 2);
-            for(String line; (line = readExactly(br, READER_BLOCK_LENGTH)) != null ;  ) {
+            for(String line; (line = readExactly(br, READER_BLOCK_LENGTH)) != null;  ) {
                 tempArray = line.split("");
 
                 for(int i=1;i<tempArray.length;i++){
@@ -39,13 +34,11 @@ public class FindSoundCloudLogo {
                 for (int i=0;BITMAP_SIZE+i<READER_BLOCK_LENGTH;i++) {
                     topTen = isTopTen(calcDelta(copyOfRange(intArray, i, BITMAP_SIZE+i)),bigIndex, topTen);
                     bigIndex++;
-                }
 
-                break;
+                }
+                bigIndex += 84;
+                //break;
             }
-            int [] beta = {2,2,7,9,7,2,9,0,0,9,1,4,8,7,7,1,2,3,9,6,7,2,9,9,3,9,2,9,5,7,6,2,4,7,2,9,0,3,2,8,8,8,4,9,2,7,5,3,7,7,6,8,3,3,0,3,1,2,7,3,5,9,8,8,4,3,6,6,5,7,7,9,7,3,6,7,1,4,2,2,9,4,6,4};
-            int [] alfa = {9,0,0,4,9,9,9,3,9,5,3,2,2,1,5,3,6,2,2,7,4,8,4,7,6,6,0,3,6,1,3,6,7,7,6,9,7,9,7,8,5,6,7,3,8,6,5,8,4,6,7,0,9,3,6,6,7,9,5,8,8,5,8,3,7,8,8,7,9,5,6,2,5,9,4,6,4,6,4,8,9,1,3,7};
-            System.out.println("Beta test:" + calcDelta(beta));
 
             printTopTenLogos(topTen);
             System.out.println("finished.");
@@ -97,7 +90,7 @@ public class FindSoundCloudLogo {
     }
 
     private static void printTopTenLogos(FoundLogos[] topTen) {
-        int [] sequenceArray = new int[BITMAP_SIZE];
+        int [] sequenceArray;
         for (int i=0;i<topTen.length;i++){
             sequenceArray = getSequence(topTen[i].getOffset());
             System.out.println("Position: "+(i+1)+" delta:"+topTen[i].getDelta()+" offset:"+topTen[i].getOffset());
